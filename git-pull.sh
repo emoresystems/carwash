e9c12a67780d651bd5080c6edad62a9e797b3654
nicholus munene <nscrowler39@gmail.com>
2024-12-13 12:00:45 +0300

    redo html only

--- git-pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ~/public_html    # Change this path to your project directory
-git pull origin master # Adjust "main" if your branch has a different name
-
-
--- resources/views/index.blade.php
+++
@@ -1,8 +0,0 @@
-
-        @include('layouts.header')
-        @include('layouts.topslider')
-        @include('layouts.topcards')
-        @include('layouts.body');     
-        @include('layouts.footer')   
-
-        	
