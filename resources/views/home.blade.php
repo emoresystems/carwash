25bee0c829d332ecdf5caa7d699362ec013615a6
nicholus munene <nscrowler39@gmail.com>
2024-12-13 11:32:47 +0300

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <title>Emore Systems Admin</title>
+  <title>Emore Systems Admiin</title>
   <link rel="shortcut icon" type="image/png" href="/img/emore-removebg-preview.png" />
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
